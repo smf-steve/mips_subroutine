5d571a053eeda827f166922f0c95653d292bb9be
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-11-17 15:22:49 -0800

    Added    Scanner class to read strings, etc    Memory, Stack and string stack    Type conversions
    Removed pos_msb  (not an OS interface thing)

--- java/MIPS_OS_Interface.java
+++ java/MIPS_OS_Interface.java
@@ -1,24 +1,135 @@
+import java.util.Scanner;
+import java.lang.Math;
+//import java.util.*; 
+//import java.util.regex.Pattern; 
 
 class MIPS_OS_Interface {
 
+  static Scanner stdin = new Scanner(System.in);
+
+  // Manage the special registers
+  // Note that these variables may differ from the main class register declarations
+  final static int $zero = 0;
+  static int $v0 = -1; 
+  static int $v1 = -1; 
+  static int $gp = -1;
+  static int $fp = -1;
   static int $sp = -1;
-  static int[] stack= new int[256];
+  static int $ra = -1;
 
 
-  public void push(int register) {
-    $sp = $sp + 1;
-    stack[$sp] = register;
+  public static void read(int fd, int buffer, int size) {
+    // restictied to fd = 0, size = 4;
+     Scanner _scanner = stdin;
+     if (fd != 0 || size != 1) {
+       return;
+     }
+     if (stdin.hasNext() == true) {
+       MEM[buffer] = stdin.nextByte();
+       print_d(MEM[buffer]);
+       print_ci('\n');     print_ci('\n');
+       $v0 = 1;
+     } else {
+       $v0 = 0;
+     }
   }
-  public int pop() {
-    int x = stack[$sp];
-    $sp = $sp - 1; 
-    return x;
+
+  public static void read(int fd, int buffer[], int size) {
+    // restictied to fd = 0, size = 4;
+     Scanner _scanner = stdin;
+     if (fd != 0 || size != 4) {
+       return;
+     }
+     if (stdin.hasNext()) {
+       buffer[0] = stdin.nextInt();
+       $v0 = 4;
+     } else {
+       $v0 = 0;
+     }
   }
-  public void print_d(int register) {
+
+  // MEMORY
+  static int sbrk_p = 0;
+  static byte[] MEM = new byte[1024];
+  static int[] stack = new int[256];
+  static char[][] str_stack = new char[25][];
+
+
+
+  //  TYPE Conversion
+  public static int u_byte(byte value) {
+    return Byte.toUnsignedInt(value);
+  }
+  public static int s_byte(byte value) {
+    return value;
+  }
+
+  public static int u_half(short value) {
+    return Short.toUnsignedInt(value);
+  }
+  public static int s_half(short value) {
+    return value;
+  }
+
+  public static int retval() {
+    return $v0;
+  }
+
+
+  public static void sbrk(int size) {
+    $v0 = sbrk_p;
+    sbrk_p += size;
+  }
+
+  public static void read_d() {
+    $v0 = stdin.nextInt();
+  }
+
+  public static void read_c() {
+    String str = stdin.findInLine(".");
+
+    if (str == null) {
+      $v0 = '\0';
+    }
+    else {
+      $v0 = str.charAt(0);
+    }
+  }
+
+  public static void read_s(char [] A, int count) {
+    String str;
+    char [] temp;
+
+    str = stdin.nextLine();
+    temp = str.toCharArray();
+
+    for (int i=0; i< str.length(); i++) {
+       A[i] = temp[i];
+    }
+
+    $v0 = str.length();
+  }
+
+
+
+  public static void read_x() {
+    $v0 = stdin.nextInt(16);
+  }
+
+  public static void read_o() {
+    $v0 = stdin.nextInt(8);
+  }
+
+  public static void read_t() {
+    $v0 = stdin.nextInt(2);
+  }
+
+  public static void print_d(int register) {
     System.out.printf("%d", register);
     return;
   }
-  public void print_di(int immediate  ) {
+
+  public static void print_di(int immediate  ) {
     System.out.printf("%d", immediate);
     return;
   }
@@ -37,6 +148,26 @@ class MIPS_OS_Interface {
     return;
   }
 
+  public static void print_s(char [] register) {
+    // The char [] has a 
+
+    int index;
+    for (index=0; index < register.length; index++ ) {
+       if (register[index] == '\0' ) break;
+       System.out.printf("%c", register[index]);
+     }
+    return;
+  }
+
+  public static void print_x(int register) {
+    System.out.printf("%x", register);
+    return;
+  }
+
+  public static void print_xi(int immediate  ) {
+    System.out.printf("%x", immediate);
+    return;
+  }
   public static void print_t(int register) {
 
     StringBuilder binaryValue = new StringBuilder();
@@ -72,40 +203,28 @@ class MIPS_OS_Interface {
   }
 
 
-  public static int u_byte(byte value) {
-    return Byte.toUnsignedInt(value);
+  public void push(char [] register) {
+    $sp = $sp + 1;
+    str_stack[$sp] = register;
   }
-  public static int s_byte(byte value) {
-    return value;
-  }
-
-  public static int u_half(short value) {
-    return Short.toUnsignedInt(value);
-  }
-  public static int s_half(short value) {
-    return value;
+  public char [] pop( char [] dumb ) {
+    char [] x = str_stack[$sp];
+    $sp = $sp - 1; 
+    //$v0 = x;
+    return x;
   }
 
- static int pos_msb(int $a0){
-            // $a0 : original_number
-            int $v0; // : return value
 
-            int counter;
-            int number;
-
-            counter = $zero;
-            number = $a0;
-     loop:  for(; number != $zero ;) {
-               counter ++;
-               number = number >> 1;
-               continue;
-            }
-     done:  ;
-            $v0 = counter;
-
-            return $v0;
+  public void push(int register) {
+    $sp = $sp + 1;
+    stack[$sp] = register;
   }
-
+  public int pop() {
+    int x = stack[$sp];
+    $sp = $sp - 1; 
+    $v0 = x;
+    return x;
+  }
 
 }
 
