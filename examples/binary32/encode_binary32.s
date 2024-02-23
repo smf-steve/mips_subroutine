f343673603443d0f825e314934c5db28b7eee9f6
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2024-02-23 12:51:18 -0800

    adding example

--- examples/binary32/encode_binary32.s
+++ examples/binary32/encode_binary32.s
@@ -1,15 +1,55 @@
+# FILE: encode_binary32.j
+# Description:
+#   This file provides the code to convert a binary number presented in 
+#   Scientific Notation into binary32 format.  The binary32 format is as follows:
+#
+#     binary_32:   |  s  | eeee eeee | mmm mmmm mmmm mmmm mmmm mmmm |
+#                  | <1> | <-  8  -> | <-          23            -> |
+#
+#     the sign bit is placed into position 32
+#     the biassed exponent (8 bits) is placed into positions: 31 .. 24
+#     the mantissa is left-justified (within it's 23-bit field),
+#       and is placed in positions: 23 .. 1          
+#
+#  Given a the following binary number (as an example):
+#      2# + 1.1 0100 1110 0001 x 2^ - 101
+#
+#  The input for the to sub routine is as follows:
+#
+#     sign    coefficient       expon_sign   exponent
+#      '+'    2#11010011100001  '-'          2#101
+#
+#  Note: the coefficient is represented in fix-point notation in which the radix
+#        point is always located immediately to the right of the msb.
+
+                .text
                 .globl encode_binary32
                 .include "macros/syscalls.s"
-                .include "position_of_msb.s"
+                .include "macros/subroutine.s"
+                .include "macros/stack.s"
 
 
                 # The following is test input to help you in your initial debugging
+                # When running this file via MARS.
+
                 li $a0, '+'    # 0x2b (43)
                 li $a1, 0x34E1 # (0011 0100 1110 0001) 
                 li $a2, '-'    # 0x2d   (45)
                 li $a3, 0x29   # (41)
+                jal encode_binary32
+                exit()
+
+                # Use mips_subroutine to perform additional testing
 
 
+                .macro call( %sub, %arg)
+                   save_state()
+                   push($a0)
+                   move $a0, %arg
+                   jal %sub
+                   pop($a0)
+                   restore_state()
+                .end_macro
 
 encode_binary32: nop
                 # Prototype:  encode_binary32( sign, coefficient, expon_sign, expon )
@@ -18,7 +58,7 @@ encode_binary32: nop
                 # a1: coefficient (representing, in total,:  1.\<mantissa\>) 
                 # a2: expon_sign -- an ASCII character
                 # a3: exponent (unbiased)
-                # v0: the encoded binary32 value
+                # v0: encoding -- the encoded binary32 value
         
                 # Initial Register Allocation
                 # t0: sign
@@ -30,40 +70,48 @@ encode_binary32: nop
                 # t4: encoded_sign
                 # t5: encoded_mantissa
                 # t6: encoded_exponent;
-                # t7: pos_msb
-                # t8: negative_sign
-                # t9: reposition
+                # t7: position -- the location of the msb of the coefficient
+                # t8: coefficient_shift
+                # t9: negative_sign
+
+                # The following do not get registers since they are all immediates
+                #    used with the shift operations.  They are provide here for 
+                #    as assemble literals:
+
+                .eqv bias 127                   #  final int bias           = 127;  // As defined by the spec
+                .eqv sign_shift 31              #  final int sign_shift     =  31;  //   << (8 + 23 )
+                .eqv expon_shift 23             #  final int expon_shift    =  23;  //   << (23)
+                .eqv mantissa_shift  9          #  final int mantissa_shift =   9;  //  >>> (1 + 8)  // the mantissa is left-justified
+                                                #  final int $zero          =   0;  
 
 
-                # Demarshall your input arguments
+                # Demarshal your input arguments
                 move $t0, $a0
                 move $t1, $a1
                 move $t2, $a2
                 move $t3, $a3
 
-                # Insert a copy of your Java TAC code here and then comment it out!
+                #/////////////////////////////////////////////////////////
+                #// BEGIN CODE of INTEREST
+                #/////////////////////////////////////////////////////////
 
-                                        # /////////////////////////////////////////////////////////
-                                        # // BEGIN CODE of INTEREST
-                                        # /////////////////////////////////////////////////////////
-                li $t8, '-'             # final int negative_sign = '-';
-                    
+
+                li $t9, '-'             # negative_sign = '-';  // Define the value
+
                                         # /////////////////////////////////////////////////////////
                                         # // 1. Encode each of the three fields of the floating point format:
                     
                                         # // 1.1 Sign Encoding
                                         # //     - Based upon the sign, encode the sign as a binary value
-                bne $t0, $t8, alt1      # if (sign == negative_sign) {
-    cons1:      nop                     #   ;
-                li $t4, 1               #   encoded_sign = 0b1;
-                b done1                 #   // break;
-                # End of Block          # } else {
-
-    alt1:       nop                     #   ;
-                li $t4, 0               #   encoded_sign = 0b0;
-                b done1                 #   //break;
-                # End of Block          # }
-
+                bne $t0, $t9, alt1      # if (sign == negative_sign) {
+    cons1:        nop                   #   ;
+                  li $t4, 1             #   encoded_sign = 0b1;
+                  b done1               #   // break;
+                                        # } else {
+    alt1:         nop                   #   ;
+                  li $t4, 0             #   encoded_sign = 0b0;
+                  b done1               #   //break;
+                                        # }
     done1:      nop                     # ;
 
                     
@@ -71,32 +119,31 @@ encode_binary32: nop
                                         # //     - Make the exponed a signed quanitity
                                         # //     - Add the bias
                                         # // negative_sign = '-';
-                bne $t2, $t8, alt2      # if (expon_sign == negative_sign) {
-    cons2:      nop                     #   ;                        
-                sub $t6, $zero, $t3     #   encoded_exponent = - exponent;
-                b done2                 #   // break;
-                # End of Block          # } else {
-
-    alt2:       nop                     #   ;
-                move $t6, $t3           #   encoded_exponent = exponent;
-                b done2                 #   // break;
-                # End of Block          # }
-
+                bne $t2, $t9, alt2      # if (expon_sign == negative_sign) {
+    cons2:        nop                   #   ;                        
+                  sub $t6, $zero, $t3   #   encoded_exponent = - exponent;
+                  b done2               #   // break;
+                                        # } else {
+    alt2:         nop                   #   ;
+                  move $t6, $t3         #   encoded_exponent = exponent;
+                  b done2               #   // break;
+                                        # }
     done2:      nop                     # ;
+
                                         # // Add the bias to the exponent
-                addi $t6, $t6, 127      # encoded_exponent = encoded_exponent + binary32_bias; // 127
+                addi $t6, $t6, bias     # encoded_exponent = encoded_exponent + bias; // 127
                                         #                    
                                         # 
                                         # // 1.3  Mantissa Encoding
-                                        # //      - Determ the number of bits in the coefficient
+                                        # //      - Determine the number of bits in the coefficient
                                         # //      - Shift the coefficient to the left to obtain the mantissa
-                position_of_msb($t1)    # pos_msb = position_most_significant_bit(coefficient);
+
+                call pos_msb $t1        # //  position = pos_msb(coefficient);
                 move $t7, $v0            
                                         # {
-                li $t9, 32              #    int reposition = 32;
-                sub $t9, $t9, $t7       #    reposition = reposition - pos_msb;
-                addi $t9, $t9, 1        #    reposition = reposition + 1;
-                sllv $t5, $t1, $t9      #    encoded_mantissa = coefficient << reposition;
+                li $t8, 33              #    int coefficient_shift = 33;
+                sub $t8, $t8, $t7       #    coefficient_shift = coefficient_shift - pos_msb;
+                sllv $t5, $t1, $t8      #    encoded_mantissa = coefficient << coefficient_shift;
                                         # }
                      
                                         # /////////////////////////////////////////////////////////
@@ -107,19 +154,43 @@ encode_binary32: nop
                                         # 
                                         # /////////////////////////////////////////////////////////
                                         # // 3. Merge the pieces together
-                or $v0, $t4, $t5        # $v0 = encoded_sign | encoded_exponent;
-                or $v0, $v0, $t6        # $v0 = $v0 | encoded_mantissa;
-                                        # /////////////////////////////////////////////////////////
-                                        # // END CODE of INTEREST
-                                        # /////////////////////////////////////////////////////////
-
-                # Provide some output for initial testing
-                print_t($v0)
-                print_ci('\n')
+                or $v0, $t4, $t5        # encoding = encoded_sign | encoded_exponent;
+                or $v0, $v0, $t6        # encoding = encoding | encoded_mantissa;
+   
 
                 # Marshall  your output arguements
                 move $v0, $v0
 
                 jr $ra
-                # With the test input, the value of v0 should be:
-                # v0: 0x2B538400
+
+                #/////////////////////////////////////////////////////////
+                #// END CODE of INTEREST
+                #/////////////////////////////////////////////////////////
+
+
+
+pos_msb:  nop                           # int pos_msb(int number)
+        # a0:  number
+        # v0:  counter -- return value
+        # t0:  number
+
+        # Demarshal your input arguments
+        move $t0, $a0                   
+
+        li $v0, 0                       # counter = 0;
+init:   nop                             # init:  ;
+loop:   beq $t0, $zero, done            # loop:  for(; number != zero ;) {
+body:     nop                           # body:     ;
+          addi $v0, $v0, 1              #           counter ++;
+          srl $t0, $t0, 1               #           number = number >> 1;
+        b loop                          #           contine loop;
+                                        #        }
+done:   nop                             # done:  nop
+        move $v0, $v0                   #        return counter;
+        jr $ra
+
+
+
+
+
+
